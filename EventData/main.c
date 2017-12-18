1f397bd258a4be67f8afb9e12b0785d802c97d7a
SpitzR2D2 <34648055+SpitzR2D2@users.noreply.github.com>
2017-12-18 12:58:20 +0100

    Add files via upload

---
+++ EventData/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "EventData.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+/* Test unitaire : Structure de données
+
+structure EventData
+{
+Nom
+Salle
+Adresse salle
+Date
+}
+
+// 1 : Lire le nom de l'event => Erreur car pas encore de nom
+// 2 : setnom(xxx);
+	getnom(xxx); => pas d'erreur
+	*/
